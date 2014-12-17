be72b667ecc94ccb2169de2f45bca9c49519baee
Melody Serra <melodyserra@gmail.com>
2014-12-16 22:14:55 -0800

    More work

--- app/controllers/nonprofit_controller.rb
+++ app/controllers/nonprofit_controller.rb
@@ -1,7 +1,17 @@
 class NonprofitController < ApplicationController
   def index
     response=HTTParty.get('https://projects.propublica.org/nonprofits/api/v1/search.json?q=san+francisco')
-    puts response.body
-    @body=response.body
+    # puts response.body
+    # @body=response.body
+
+    res_org_info = response["filings"]
+
+    org_info = []
+
+    res_org_info.each do |org|
+      org_info << { name: org["organization"]["name"], city: org["organization"]["city"], state: org["organization"]["state"] }
+    end
+
+    @orgs = org_info
   end
 end
