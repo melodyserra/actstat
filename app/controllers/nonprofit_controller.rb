9977e8fe55a0af2248f289ad3619e7307bff0fa6
Melody Serra <melodyserra@gmail.com>
2014-12-16 23:40:15 -0800

    trying to integrate nonprofit api

--- app/controllers/nonprofit_controller.rb
+++ app/controllers/nonprofit_controller.rb
@@ -1,15 +1,24 @@
 class NonprofitController < ApplicationController
   def index
-    response=HTTParty.get('https://projects.propublica.org/nonprofits/api/v1/search.json?q=san+francisco')
-    # puts response.body
-    # @body=response.body
+    # response=HTTParty.get('https://projects.propublica.org/nonprofits/api/v1/search.json?q=san+francisco')
 
-    res_org_info = response["filings"]
+    # res_org_info = response["filings"]
 
+    # org_info = []
+
+    # res_org_info.each do |org|
+    #   org_info << { name: org["organization"]["name"], city: org["organization"]["city"], state: org["organization"]["state"] }
+    # end
+
+    # @orgs = org_info
+
+    response = HTTParty.get("https://data.guidestar.org/v1/search.json?q=organization_name:guide*")
+
+    res_org_info = response[""]
     org_info = []
 
     res_org_info.each do |org|
-      org_info << { name: org["organization"]["name"], city: org["organization"]["city"], state: org["organization"]["state"] }
+      org_info << { name: org["Organization Name"]}
     end
 
     @orgs = org_info
